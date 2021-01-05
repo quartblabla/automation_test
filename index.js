8ee3bc5d90965a7ba86e36a6c3a433f045a05537
Vasyl <quartblabla@gmail.com>
2021-01-05 12:21:50 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+let driver_fx = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+
+let driver_chr = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+searchTest(driver_fx);
+searchTest(driver_chr);
+
+function searchTest(driver) {
+  driver.get('http://www.google.com');
+  driver.findElement(By.name('q')).sendKeys('webdriver');
+
+  driver.sleep(3000).then(function() {
+    driver.findElement(By.name('q')).sendKeys(webdriver.Key.ENTER);
+  });
+
+  driver.findElement(By.name('btnK')).click();
+
+  driver.sleep(20000).then(function() {
+    driver.getTitle().then(function(title) {
+      if(title === 'webdriver - Google Search') {
+        console.log('Test passed');
+      } else {
+        console.log('Test failed');
+      }
+      driver.quit();
+    });
+  })}
